8815c696c1973ba30d52c24eb6b4b799a7dc324f
PRASHANTH-kore <prashanthkore234@gmail.com>
2022-03-31 00:10:44 +0530

    Updates

---
+++ precache-manifest.bf066aaa766960143d610174c4ec7028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef3537e797d9fd705be6",
+    "url": "/new-file/static/css/main.e5a74db1.chunk.css"
+  },
+  {
+    "revision": "ef3537e797d9fd705be6",
+    "url": "/new-file/static/js/main.ef3537e7.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/new-file/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "9ca277861acbabcf875f",
+    "url": "/new-file/static/js/2.9ca27786.chunk.js"
+  },
+  {
+    "revision": "c9c1afee958c735c5417",
+    "url": "/new-file/static/js/runtime~main.c9c1afee.js"
+  },
+  {
+    "revision": "dd72747e2ad7bc1f1b8da78ba3df6def",
+    "url": "/new-file/index.html"
+  }
+];
